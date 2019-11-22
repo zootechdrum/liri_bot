19b288ed47fd0e2c23d1cdaa2cdc755ea2858b13
Cesar Gomez <zootechdrum@gmail.com>
2019-11-21 21:25:35 -0800

    added files to node project

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # liri_bot
 This is a command line application.
+
+## Technologies Used (including NPM packages)
+
+1. Node.js
+2. axios
+3. Javascript
+
---
+++ liri.js
@@ -0,0 +1,7 @@
+require("dotenv").config();
+var keys = require("./keys.js");
+var Spotify = require('node-spotify-api');
+
+var spotify = new Spotify(keys.spotify);
+
+console.log(spotify)
