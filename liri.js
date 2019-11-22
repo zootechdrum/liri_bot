d84dc13e0f6bc2192fa5f8234fc0a3d585761b64
Cesar Gomez <zootechdrum@gmail.com>
2019-11-22 10:06:44 -0800

    added conditional in the case array comes back emtpy

--- liri.js
+++ liri.js
@@ -3,6 +3,7 @@ var keys = require("./keys.js");
 var Spotify = require('node-spotify-api');
 var axios = require('axios');
 var chalk = require('chalk');
+var moment = require('moment');
 
 var spotify = new Spotify(keys.spotify);
 
@@ -22,12 +23,16 @@ switch (execCommand) {
 function bandIntown(artist){
     axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
     .then(function (response){
-        console.log(response.data[0].venue.name)
-        console.log("The Venue will be " + response.data[0].venue.name)
-        console.log("--------------------------------------------")
-        console.log("The concert will be held in  " + response.data[0].venue.city)
-        console.log("--------------------------------------------")
-        console.log("The date the event will held is " + response.data[0].datetime) 
+        //conditional if response.data is equal to empty array
+        if(response.data = []){
+            console.log(chalk.white.bgBlack.bold("Nothing matches your Criteria"))
+        }else{
+        console.log(chalk.white.bgBlack.bold("The Venue will be " + response.data[0].venue.name))
+        console.log(chalk.red("--------------------------------------------"))
+        console.log(chalk.white.bgBlack.bold("The concert will be held in  " + response.data[0].venue.city))
+        console.log(chalk.red("--------------------------------------------"))
+        console.log(chalk.white.bgBlack.bold("The date the event will held is " + moment(response.data[0].datetime).format('LLLL'))) 
+        }
     });
 
 }
