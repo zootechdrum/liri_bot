8b6689f59df464567c74b2d37dc4d8071f96abf0
Cesar Gomez <zootechdrum@gmail.com>
2019-11-21 21:50:04 -0800

    added axios and registered for band api

--- liri.js
+++ liri.js
@@ -1,16 +1,27 @@
 require("dotenv").config();
 var keys = require("./keys.js");
 var Spotify = require('node-spotify-api');
+var axios = require('axios');
 
 var spotify = new Spotify(keys.spotify);
 
-var userInput = process.argv[2];
+var execCommand = process.argv[2];
+var searchQ = process.argv[3];
 
 //Checks what the user inputted in terminal.
 
-switch (userInput) {
+switch (execCommand) {
     case "concert-this":
-        console.log("concert")
+        bandIntown(searchQ)
         break;
 }
 
+
+
+function bandIntown(artist){
+    axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
+    .then(function (response){
+        console.log(response)
+    });
+
+}
