9a98ea45037934dbd0bca5baedee89bd76faa247
Cesar Gomez <zootechdrum@gmail.com>
2019-11-21 22:07:33 -0800

    Successfuly accessed relveant info for bands in town api

--- liri.js
+++ liri.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 var keys = require("./keys.js");
 var Spotify = require('node-spotify-api');
 var axios = require('axios');
+var chalk = require('chalk');
 
 var spotify = new Spotify(keys.spotify);
 
@@ -21,7 +22,12 @@ switch (execCommand) {
 function bandIntown(artist){
     axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
     .then(function (response){
-        console.log(response)
+        console.log(response.data[0].venue.name)
+        console.log("The Venue will be " + response.data[0].venue.name)
+        console.log("--------------------------------------------")
+        console.log("The concert will be held in  " + response.data[0].venue.city)
+        console.log("--------------------------------------------")
+        console.log("The date the event will held is " + response.data[0].datetime) 
     });
 
 }
